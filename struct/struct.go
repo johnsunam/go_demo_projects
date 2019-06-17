bde6e42d6bc84fb098e2fed4090117d0ce3aa721
John Sunam <johnsunam@SPMBP084.local>
2019-06-17 14:02:36 +0545

    struct implemented examples

--- struct/struct.go
+++ struct/struct.go
@@ -12,5 +12,31 @@ type CMember struct {
 }
 
 func main() {
-	fmt.Println("in function")
+	cm := CMember{
+		name:    "John sunam",
+		age:     26,
+		address: "Itahari",
+		rank:    1,
+	}
+
+	cmp := &cm
+	cmp.address = "Itahari, kathmandu"
+	var crew []CMember
+	crew = append(crew, cm)
+
+	for i, v := range crew {
+		fmt.Println(i, v.name)
+	}
+	fmt.Println(cm)
+	var m map[string]CMember
+	m = make(map[string]CMember)
+	m["keving"] = cm
+
+	m = map[string]CMember{
+		"first, ":  CMember{name: "first name", address: "first address"},
+		"second, ": CMember{name: "second name", address: "second address"},
+	}
+	for i, v := range m {
+		fmt.Println("key::", i, "value::", v)
+	}
 }
